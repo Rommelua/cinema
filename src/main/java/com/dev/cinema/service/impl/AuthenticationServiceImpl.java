dd4d519d4e535322b72bea3370c67239c8675050
Leonid <rommel@ukr.net>
2020-10-09 11:46:44 +0300

    AuthenticationServiceImpl refactored

--- src/main/java/com/dev/cinema/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/dev/cinema/service/impl/AuthenticationServiceImpl.java
@@ -29,7 +29,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User register(String email, String password) {
         User user = new User(email, password);
-        user = userService.add(user);
+        userService.add(user);
         cartService.registerNewShoppingCart(user);
         return user;
     }
