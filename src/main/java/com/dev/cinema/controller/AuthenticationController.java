21e194f8d552ebe9eebe62773d7e05e126386efc
Leonid <rommel@ukr.net>
2020-10-22 19:18:49 +0300

    AuthenticationController fixed

--- src/main/java/com/dev/cinema/controller/AuthenticationController.java
+++ src/main/java/com/dev/cinema/controller/AuthenticationController.java
@@ -1,29 +1,26 @@
 package com.dev.cinema.controller;
 
-import com.dev.cinema.model.User;
 import com.dev.cinema.model.dto.UserRequestDto;
-import com.dev.cinema.service.interfaces.UserService;
+import com.dev.cinema.service.interfaces.AuthenticationService;
 import com.dev.cinema.service.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
-    private final UserService userService;
+    private final AuthenticationService service;
     private final UserMapper userMapper;
 
     @Autowired
-    public AuthenticationController(UserService userService, UserMapper userMapper) {
-        this.userService = userService;
+    public AuthenticationController(AuthenticationService service, UserMapper userMapper) {
+        this.service = service;
         this.userMapper = userMapper;
     }
 
     @PostMapping("/register")
     public void register(@RequestBody UserRequestDto dto) {
-        User user = userMapper.unmapUser(dto);
-        userService.add(user);
+        service.register(dto.getEmail(), dto.getPassword());
     }
 }
